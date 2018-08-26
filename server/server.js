63f26fe5725bd836e3ef411e1fd97f91cd9b43da
Manguo1401 <aaronleehartnell@gmail.com>
2018-08-26 19:53:03 +0300

    patch a todo

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 const {ObjectID} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
@@ -63,6 +64,35 @@ app.delete('/todos/:id', (req, res) => {
         res.status(200).send({todo});
     }).catch((err) => {
         res.status(404).send();
+    });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    // Chooses the properties the client can update (blocks the others like completedAt for example)
+    let body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {
+        $set: body
+    }, { new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((err) => {
+        res.status(400).send();
     });
 });
 
